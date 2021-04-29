31c876f4a154809648218e6f3f8d151396ddc6f3
gby1990 <36633416+gby1990@users.noreply.github.com>
2021-04-29 21:12:21 +0200

    initial commit for a simple proj

---
+++ iRAM/src/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../src/hello.c 
+
+OBJS += \
+./src/hello.o 
+
+C_DEPS += \
+./src/hello.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+src/%.o: ../src/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: TriCore C Compiler'
+	tricore-gcc -c -I"../h" -fno-common -Os -g3 -W -Wall -Wextra -Wdiv-by-zero -Warray-bounds -Wcast-align -Wignored-qualifiers -Wformat -Wformat-security -pipe -DAPPKIT_TC265B -fshort-double -mcpu=tc26xx -mversion-info -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ src/hello.c
@@ -0,0 +1,12 @@
+/*====================================================================
+* Project:  Board Support Package (BSP) examples
+* Function: empty main function
+*
+* Copyright HighTec EDV-Systeme GmbH 1982-2019
+*====================================================================*/
+#include <stdlib.h>
+
+int main(void)
+{
+	return EXIT_SUCCESS;
+}
